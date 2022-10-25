19f0d67296d3740eea76e1d9dc9a19fb48f8ecbb
Chris-Skipsey1 <K1915275@kingston.ac.uk>
2022-10-25 10:09:15 +0100

    User story complete (not Refactored)

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,43 @@
+import { useState, useEffect } from 'react';
+
 function Home() {
-    return (
-        <h2>Home page</h2>
-        
-      
-    );
+  // Initialisation
+  const loggedInUserID = 5;
+  const endpoint = `/medicines/clients/${loggedInUserID}`;
+
+  //State
+  const [medicines, setMedicines] = useState(null);
+  const [loadingMessage, setLoadingMessage] = useState('Loading medicines ...');
+
+  //Context
+  //Methods
+  const apiCall = async (endpoint) => {
+  const URL = 'http://localhost:5000/api';
+  const endpointAddress = URL + endpoint;
+  const response = await fetch(endpointAddress);
+  const result = await response.json();
+  setMedicines(result);
   }
- 
+useEffect(() => {apiCall(endpoint) }, [endpoint]);
+
+
+
+  //View
+  return (
+    <section>
+      <h2>Home page</h2>
+      {
+        !medicines
+          ? <p>{loadingMessage}</p>
+          : medicines.length === 0
+            ? <p>No medicines found</p>
+            : medicines.map((medicine) =>
+              <p key={medicine.MedicineID}>{medicine.MedicineID} {medicine.MedicineName}</p>
+            )
+      }
+    </section>
+
+  );
+}
+
 export default Home;
